64f3eac76e7be8b8efddca238126f301d3311f29
James Socol <me@jamessocol.com>
2013-02-18 15:17:54 -0500

    Init.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+import jsonview
+
+
+setup(
+    name='django-jsonview',
+    version=jsonview.__version__,
+    description='Always return JSON from your Django view.',
+    author='James Socol',
+    author_email='me@jamessocol.com',
+    url='https://github.com/jsocol/django-jsonview',
+    license='Apache v2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
